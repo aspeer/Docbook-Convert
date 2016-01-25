d4180014468baaf42e986579e110d0a744f85825
Andrew Speer <andrew.speer@isolutions.com.au>
2016-01-25 15:22:05 +1030

    VERSION increment: 0.003

--- lib/Docbook/Convert/Tag.pm
+++ lib/Docbook/Convert/Tag.pm
@@ -40,7 +40,7 @@ use base Docbook::Convert::Base;
 #  Version information in a format suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='0.002';
+$VERSION='0.003';
 
 
 #  Make synonyms
