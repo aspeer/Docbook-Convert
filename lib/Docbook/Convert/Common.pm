d1338772524fde716bce2dce6d64a147dbee6315
Andrew Speer <andrew.speer@isolutions.com.au>
2017-08-24 17:07:52 +0930

    VERSION increment: 0.016

--- lib/Docbook/Convert/Common.pm
+++ lib/Docbook/Convert/Common.pm
@@ -40,7 +40,7 @@ use base Docbook::Convert::Base;
 #  Version information in a format suitable for CPAN etc. Must be
 #  all on one line
 #
-$VERSION='0.015';
+$VERSION='0.016';
 
 
 #  Make synonyms
